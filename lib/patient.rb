2feb3adc81f824f8929f6886c8df6a2391f4f533
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-23 22:47:08 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,28 +1,31 @@
 class Patient
-    attr_accessor :name
+  attr_accessor :name
 
-    @@all = []
+  @@all = []
 
-    def initialize(name)
-        @name = name
-        @appointments = []
-        @@all << self
-    end
+ def initialize(name)
+   @name = name
+   @@all << self
+ end
 
-    def self.all
-      @@all
-    end
+ def self.all
+   @@all
+ end
 
-    def add_appointment(appointment)
-        @appointments << appointment
-        appointment.patient = self
-    end
+ def new_appointment(doctor, date)
+    Appointment.new(date, self, doctor)
+  end
 
-    def appointments
-        @appointments
-    end
+  def appointments
+     Appointment.all.select do |appointment|
+       appointment.patient == self
+     end
+   end
 
-    def doctors
-        self.appointments.collect { |appointment| appointment.doctor }
+
+   def doctors
+      appointments.collect do |appointment|
+        appointment.patient
+      end
     end
 end
