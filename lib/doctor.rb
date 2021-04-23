cdacb922bd8444bab61120a7ef55e5fe7b9ccb92
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-23 20:36:53 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+  attr_accessor :name, :appointments, :patient
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    #@appointments = []
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    Appointment.new(date, self, patient)
+  end
+
+ def appointments
+    Appointment.all.select do |app|
+    app.doctor == self
+  end
+end
+
+def patients
+  appointments.collect do |app|
+    app.patient
+  end
+end
+end
