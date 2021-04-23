43699444ea7f8ab501c1c821cb39572d609bbcbc
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-23 20:55:14 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,16 +2,16 @@ class Doctor
   attr_accessor :name, :appointments, :patient
 
   @@all = []
-  def self.all
-    @@all
-  end
+
   def initialize(name)
     @name = name
     @appointments = []
     @@all << self
   end
 
-
+  def self.all
+    @@all
+  end
 
   def new_appointment(date, patient)
      appointment = Appointment.new(patient, date, self)
